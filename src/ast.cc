399d6f4b46801867b7f5eea7706474bb9e209dfd
Tarcísio G. Rodrigues <tarcisio.genaro@gmail.com>
2014-05-14 08:48:48 -0300

    Included some header just for testing.
    Also force boost library linking to be static.

--- src/ast.cc
+++ src/ast.cc
@@ -1,4 +1,13 @@
+#include <iostream>  
 #include <cstdlib>
+
+#include <cppcms/application.h>  
+#include <cppcms/applications_pool.h>  
+#include <cppcms/service.h>  
+#include <cppcms/http_response.h>  
+
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
 
 int main(int argc, char **argv)
 {
